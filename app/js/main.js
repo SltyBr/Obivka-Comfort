387ba1c20c33acdf22d5ad48b3fa2063c1a069b5
SaltyBr <SaltyBrain@yandex.ru>
2021-02-19 20:45:59 +0300

    делаю адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -17,7 +17,7 @@ let mySwiper = new Swiper(slider, {
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-    // when window width is >= 320px
+
     140: {
       slidesPerView: 1,
       spaceBetween: 10
@@ -30,12 +30,12 @@ let mySwiper = new Swiper(slider, {
       slidesPerView: 2,
       spaceBetween: 20
     },
-    // when window width is >= 480px
-    740: {
+
+    640: {
       slidesPerView: 3,
       spaceBetween: 30
     },
-    // when window width is >= 640px
+
     960: {
       slidesPerView: 4,
       spaceBetween: 40
